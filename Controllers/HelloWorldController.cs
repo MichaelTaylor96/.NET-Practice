52c4b073659b67168ae0f7c6d3093546b3899f0a
Michael Taylor <michael96taylor@gmail.com>
2019-06-17 16:21:53 -0400

    Added first controller

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        public IActionResult Index()
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
         {
-            return View();
+            return "This is my default action...";
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
